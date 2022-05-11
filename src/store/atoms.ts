ad7ac6e9ca77556097bfec206fe7a2ae6ae372dd
lazy-sky <1cielo@naver.com>
2022-05-12 01:28:53 +0900

    feat: Make atoms about searching

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -3,6 +3,16 @@ import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist();
 
+export const searchKeywordState = atom({
+  key: 'searchKeyword',
+  default: ''
+});
+
+export const searchResults = atom({
+  key: 'searchResults',
+  default: []
+});
+
 export const favoritesState = atom({
   key: 'gripFavorites',
   default: [],
