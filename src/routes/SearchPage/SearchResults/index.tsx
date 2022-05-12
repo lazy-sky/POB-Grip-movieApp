8b726916c3a5c9a66dce8b107ab64a3b1fa225e6
lazy-sky <1cielo@naver.com>
2022-05-12 23:36:48 +0900

    fix: Fix error when there's no movie to render

--- src/routes/SearchPage/SearchResults/index.tsx
+++ src/routes/SearchPage/SearchResults/index.tsx
@@ -62,7 +62,7 @@ const SearchResults = () => {
   };
 
   return (
-    movies.length
+    movies
       ? <ul className={styles.searchResults}>
         {movies.map(movie => {
           const { imdbID, Poster, Title, Year, Type } = movie;
