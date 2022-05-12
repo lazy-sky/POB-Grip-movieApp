e32d97183c9958b0509a8213545608eeef218295
lazy-sky <1cielo@naver.com>
2022-05-12 14:25:03 +0900

    feat: Set layout of the app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,21 @@
 import GNB from 'components/GNB';
 import { Route, Routes } from 'react-router-dom';
 
-import Favorites from './Favorites';
+import FavoritesPage from './FavoritesPage';
 import styles from './routes.module.scss';
 import SearchPage from './SearchPage';
 
 const App = () => {
   return (
-    <div>
-      <GNB />
-      <div className={styles.app}>
+    <div className={styles.app}>
+      <div className={styles.pages}>
         <Routes>
           <Route path='/' element={<SearchPage />} />
           <Route path='search' element={<SearchPage />} />
-          <Route path='favorites' element={<Favorites />} />
+          <Route path='favorites' element={<FavoritesPage />} />
         </Routes>
       </div>
+      <GNB />
     </div>
   );
 };
