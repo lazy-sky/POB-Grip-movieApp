977959673de66fe8d3615f29bf70d016989978e6
lazy-sky <1cielo@naver.com>
2022-05-15 04:49:03 +0900

    refact: Separate FavoriteItem from Favorites

---
+++ src/routes/FavoritesPage/Favorites/FavoriteItem/index.tsx
@@ -0,0 +1,88 @@
+import fireAlertModal from 'components/AlertModal';
+import { useRecoilState } from 'recoil';
+import { favoritesState } from 'store/atoms';
+import { IFavorite } from 'types/movie';
+
+import NoImage from '../../../../assets/images/no-image.jpg';
+import styles from './favoriteItem.module.scss';
+
+const FavoriteItem = ({ movie }: {
+  movie: IFavorite
+}) => {
+  const [favorites, setFavorites] = useRecoilState<IFavorite[]>(favoritesState);
+
+  const { imdbID, Poster, Title, Year, Type, ratingStar } = movie;
+
+  const handleMovieClick = (clickedMovie: IFavorite) => {
+    fireAlertModal({
+      movie: { ...clickedMovie, ratingStar: 0 },
+      isAlreadySaved: true,
+      favorites,
+      setFavorites
+    });
+  };
+
+  const handleStarUpClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    id: string
+  ) => {
+    event.stopPropagation();
+
+    setFavorites(favorites.map((favorite) => favorite.imdbID === id
+      ? { ...favorite, ratingStar: Math.min(favorite.ratingStar + 1, 5)}
+      : favorite
+    ));
+  };
+
+  const handleStarDownClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    id: string
+  ) => {
+    event.stopPropagation();
+
+    setFavorites(favorites.map((favorite) => favorite.imdbID === id
+      ? { ...favorite, ratingStar: Math.max(favorite.ratingStar - 1, 0)}
+      : favorite
+    ));
+  };
+
+  return (
+    <div
+      role='button'
+      tabIndex={0}
+      onClick={() =>handleMovieClick(movie)}
+    >
+      <div className={styles.moviePoster}>
+        {Poster === 'N/A'
+          ? <img src={NoImage} alt={Title} />
+          : <img src={Poster} alt={Title} />
+        }
+      </div>
+      <div className={styles.movieInfo}>
+        <h4>{Title}</h4>
+        <div>{Year}</div>
+        <div>{Type}</div>
+        <div className={styles.movieRating}>
+          <button
+            type='button'
+            onClick={(event) => handleStarUpClick(event, imdbID)}
+          >
+            ↑
+          </button>
+          <div>
+            <div>★</div>
+            <div>{ratingStar}</div>
+          </div>
+          <button
+            type='button'
+            onClick={(event) => handleStarDownClick(event, imdbID)}
+          >
+            ↓
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FavoriteItem;
--- src/routes/FavoritesPage/Favorites/index.tsx
+++ src/routes/FavoritesPage/Favorites/index.tsx
@@ -1,11 +1,10 @@
-import fireAlertModal from 'components/AlertModal';
 import NoMovie from 'components/NoMovie';
 import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
 import { useRecoilState } from 'recoil';
 import { favoritesState } from 'store/atoms';
-import { IFavorite, IMovie } from 'types/movie';
+import { IFavorite } from 'types/movie';
 
-import NoImage from '../../../assets/images/no-image.jpg';
+import FavoriteItem from './FavoriteItem';
 import styles from './favorites.module.scss';
 
 const Favorites = () => {
@@ -38,39 +37,6 @@ const Favorites = () => {
     setFavorites(reordered);
   };
 
-  const handleMovieClick = (movie: IMovie) => {
-    fireAlertModal({
-      movie: { ...movie, ratingStar: 0 },
-      isAlreadySaved: true,
-      favorites,
-      setFavorites
-    });
-  };
-
-  const handleStarUpClick = (
-    event: React.MouseEvent<HTMLButtonElement>,
-    id: string
-  ) => {
-    event.stopPropagation();
-
-    setFavorites(favorites.map((movie) => movie.imdbID === id
-      ? { ...movie, ratingStar: Math.min(movie.ratingStar + 1, 5)}
-      : movie
-    ));
-  };
-
-  const handleStarDownClick = (
-    event: React.MouseEvent<HTMLButtonElement>,
-    id: string
-  ) => {
-    event.stopPropagation();
-
-    setFavorites(favorites.map((movie) => movie.imdbID === id
-      ? { ...movie, ratingStar: Math.max(movie.ratingStar - 1, 0)}
-      : movie
-    ));
-  };
-
   return (
     favorites && favorites.length > 0
       ? <DragDropContext onDragEnd={handleDragEnd}>
@@ -82,55 +48,19 @@ const Favorites = () => {
               className={styles.favorites}
             >
               {favorites.map((movie, index) => {
-                const { imdbID, Poster, Title, Year, Type, ratingStar } = movie;
                 return (
                   <Draggable
-                    key={`movie-${imdbID}`}
-                    draggableId={`movie-${imdbID}`}
+                    key={`movie-${movie.imdbID}`}
+                    draggableId={`movie-${movie.imdbID}`}
                     index={index}
                   >
                     {(innerProvided) => (
-                    // TODO: MovieItem으로 컴포넌트 분리
                       <li
                         ref={innerProvided.innerRef}
                         {...innerProvided.draggableProps}
                         {...innerProvided.dragHandleProps}
                       >
-                        <div
-                          role='button'
-                          tabIndex={0}
-                          onClick={() =>handleMovieClick(movie)}
-                        >
-                          <div className={styles.moviePoster}>
-                            {Poster === 'N/A'
-                              ? <img src={NoImage} alt={Title} />
-                              : <img src={Poster} alt={Title} />
-                            }
-                          </div>
-                          <div className={styles.movieInfo}>
-                            <h4>{Title}</h4>
-                            <div>{Year}</div>
-                            <div>{Type}</div>
-                            <div className={styles.movieRating}>
-                              <button
-                                type='button'
-                                onClick={(event) => handleStarUpClick(event, imdbID)}
-                              >
-                                ↑
-                              </button>
-                              <div>
-                                <div>★</div>
-                                <div>{ratingStar}</div>
-                              </div>
-                              <button
-                                type='button'
-                                onClick={(event) => handleStarDownClick(event, imdbID)}
-                              >
-                                ↓
-                              </button>
-                            </div>
-                          </div>
-                        </div>
+                        <FavoriteItem movie={movie} />
                       </li>
                     )}
                   </Draggable>
